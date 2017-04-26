ffeede4c79e64a1d3c349b8822d56e01cbb5e9a6
whnarthur <314954790@qq.com>
2017-04-26 10:09:16 +0800

    init commit

---
+++ c_plus_plus/c_plus_plus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c_plus_plus
+//
+//  Created by tomapto－lsg on 17/4/26.
+//  Copyright © 2017年 tomapto－lsg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
