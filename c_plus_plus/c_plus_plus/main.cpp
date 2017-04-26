0d2c4031309ffff8208c3893c21db3b51ec0f3e6
whnarthur <314954790@qq.com>
2017-04-26 10:21:25 +0800

    add workspace and c_plus_plus project

--- c_plus_plus/c_plus_plus/main.cpp
+++ c_plus_plus/c_plus_plus/main.cpp
@@ -2,14 +2,37 @@
 //  main.cpp
 //  c_plus_plus
 //
-//  Created by tomapto－lsg on 17/4/26.
-//  Copyright © 2017年 tomapto－lsg. All rights reserved.
-//
+//  Created by whnarthur on 17/4/26.
+//  Copyright © 2017年  All rights reserved.
+//  用于测试将'const xxx'作为'xxx'的'this'实参时丢弃了类型限定的错误
 
+#include <cctype>
+#include <string>
+#include <set>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+class A
+{
+public:
+    string m_str;
+    A(string str){m_str = str;}
+    string ToString() {return m_str;}
+    bool operator<(const A &rhs) const {return true;} //为了能使用set
+};
+
+void output(const string &str)
+{
+    cout<<str<<endl;
+}
+
+int main()
+{
+    A a("dfsfsd");
+    output(a.ToString());
+    set<A> strset;
+    strset.insert(a);
+    output(strset.begin()->ToString());
     return 0;
 }
